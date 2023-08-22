a1a3ddf7a8df384a9568f23fd0e6b20ca9b2cd5e
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-21 22:28:18 -0700

    react -- templates

--- lazydev-blog/src/App.js
+++ lazydev-blog/src/App.js
@@ -2,10 +2,16 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the blog";
+  const link = "www.fortheeco.com"
+
   return (
     <div className="App">
       <div className="content">
-        <h2>React Components</h2>
+        <h2>{title}</h2>
+        <p>{Math.random() * 10}</p>
+
+        <a href={link}>FORTHEECO</a>
       </div>
     </div>
   );
