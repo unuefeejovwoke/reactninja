b89d21414c45c536e079e7b0d6e64f93bd71c773
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-21 22:19:51 -0700

    react-- components

---
+++ lazydev-blog/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h2>React Components</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
