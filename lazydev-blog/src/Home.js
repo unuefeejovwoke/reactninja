a4d09ac7e4f82d040dfcf01dff02102657f31116
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-22 22:34:47 -0700

    using react states and outputting lists

--- lazydev-blog/src/Home.js
+++ lazydev-blog/src/Home.js
@@ -1,18 +1,19 @@
 import {useState} from 'react';
 
 const Home = () => {
-    const [name, setName] = useState('Ejoks');
-    const [age, setAge] = useState(24)
-
-    const handleClick = () =>{
-        setName('Unuefe');
-        setAge(71)
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
     return ( 
         <div className="home">
-            <h2>HomePage</h2>
-            <p>{name} is {age} years old.</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>{blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
