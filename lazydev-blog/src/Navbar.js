2a7d2ac4c9c943bc2da80233eed76102894ad711
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-22 21:51:37 -0700

    adding styles

--- lazydev-blog/src/Navbar.js
+++ lazydev-blog/src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
             <h1>The LazyDev Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <a href="/create" className="blog">New Blog</a>
             </div>
         </nav>
      );
