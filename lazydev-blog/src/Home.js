8e156b92b47e2440be244db88ef69cf6572de3c7
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-22 22:25:42 -0700

    react click events

--- lazydev-blog/src/Home.js
+++ lazydev-blog/src/Home.js
@@ -1,7 +1,18 @@
+import {useState} from 'react';
+
 const Home = () => {
+    const [name, setName] = useState('Ejoks');
+    const [age, setAge] = useState(24)
+
+    const handleClick = () =>{
+        setName('Unuefe');
+        setAge(71)
+    }
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <p>{name} is {age} years old.</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
      );
 }
