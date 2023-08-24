173a8887e5fb6f8f931302f40df26edeb76e1d10
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-23 21:47:20 -0700

    react props, func as props

--- lazydev-blog/src/Home.js
+++ lazydev-blog/src/Home.js
@@ -1,4 +1,5 @@
 import {useState} from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -6,14 +7,15 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
       ])
+
+      const handleDelete = (id) =>{
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs)
+      }
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>{blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title={"All Blogs!"} handleDelete={handleDelete} />
+            
         </div>
      );
 }
