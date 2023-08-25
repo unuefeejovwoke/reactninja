a2509511bc224a315a2aec5a7177912a9bd9c73c
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-24 21:29:17 -0700

    handling fetch errors

--- lazydev-blog/src/Home.js
+++ lazydev-blog/src/Home.js
@@ -4,22 +4,32 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
     const [isPending, setPending] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
       setTimeout(() =>{
         fetch('http://localhost:8000/blogs')
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          setBlogs(data)
-          setPending(false)
-        }) 
+          .then(res => {
+            if(!res.ok){
+              throw Error("Could not fecth the data for that resource")
+            }
+            return res.json();
+          })
+          .then(data => {
+            setBlogs(data)
+            setPending(false)
+            setError(null)
+          }) 
+          .catch(err => {
+            setPending(false)
+            setError(err.message)
+          })
       }, 1000)
     }, []);
 
     return ( 
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading ....</div>}
             {blogs && <BlogList blogs={blogs} title={"All Blogs!"} />}
         </div>
