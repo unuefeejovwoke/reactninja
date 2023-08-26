b68d4535d991968ccdaa14021d8d9199c606dd87
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-26 16:22:51 -0700

    react create blog done

--- lazydev-blog/src/Create.js
+++ lazydev-blog/src/Create.js
@@ -1,14 +1,35 @@
 import { useState } from "react";
+import {useHistory} from 'react-router-dom';
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setPending] = useState(false)
+  const history = useHistory();
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setPending(true)
+
+    fetch('http://localhost:8000/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      setPending(false)
+      history.push('/')
+    })
+  }
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input 
           type="text" 
@@ -30,7 +51,9 @@ const Create = () => {
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        { !isPending && <button>Add Blog</button>}
+        { isPending && <button>Adding Blog...</button>}
+        
       </form>
     </div>
   );
