ddeca5d50a3b2608adf399ba121c4d9de7c2877c
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-25 20:25:42 -0700

    react router links

--- lazydev-blog/src/Navbar.js
+++ lazydev-blog/src/Navbar.js
@@ -1,10 +1,12 @@
+import {Link} from 'react-router-dom'
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The LazyDev Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" className="blog">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create" className="blog">New Blog</Link>
             </div>
         </nav>
      );
